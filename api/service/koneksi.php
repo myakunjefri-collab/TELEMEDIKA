2d001b0240ef17790b594d29321b9fa6b4c83ae3
myakunjefri-collab <myakunjefri@gmail.com>
2026-04-26 14:53:53 +0700

    pindah ke telemedika

--- api/service/koneksi.php
+++ api/service/koneksi.php
@@ -1,12 +1,30 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "data_pasien_konsultasi"; 
+// Data dari TiDB Cloud
+$host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
+$port = 4000;
+$user = 'cpCH3aB6Z1ksKyu.root';
+$pass = 'N5mt7zsDSFCBBaPX';
+$db   = 'db-konsultasi';
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+// Inisialisasi mysqli
+$koneksi = mysqli_init();
 
-if (!$koneksi) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+// Menambahkan pengaturan SSL (Wajib untuk TiDB Serverless)
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
+
+// Melakukan koneksi
+$real_connect = mysqli_real_connect(
+    $koneksi, 
+    $host, 
+    $user, 
+    $pass, 
+    $db, 
+    $port, 
+    NULL, 
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$real_connect) {
+    die("Koneksi ke TiDB Cloud gagal: " . mysqli_connect_error());
 }
 ?>
